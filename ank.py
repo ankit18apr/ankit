2147bcfc6969066b4036e027a312fd8035240fba
ankit18apr <41890120+ankit18apr@users.noreply.github.com>
2018-07-31 12:15:54 +0530

    Add files via upload

---
+++ ank.py
@@ -0,0 +1,13 @@
+print("ankit")
+a=raw_input("enter a name")
+
+if a=="ankit" or a=="gaurav":
+ print("yes")
+else:
+ print ("no")
+ 
+
+l=['a','b','c','d','e','f','h','i','j']
+print(l)
+s=raw_input("enter a string")
+print(s[::-1])
